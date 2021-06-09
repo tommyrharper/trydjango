0195762c05661c662255c74fd840da524efd8cab
tommyrharper <thomasrobertharper@gmail.com>
2021-06-09 17:38:19 +0100

    Create social and about views

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
+from django.http.request import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
 def home_view(*args, **kwargs): # *args, **kwargs
   return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+
+def contact_view(*args, **kwargs):
+  return HttpResponse("<h1>Contact</h1>")
+
+def social_view(*args, **kwargs):
+  return HttpResponse("<h1>Social</h1>")
+
+def about_view(*args, **kwargs):
+  return HttpResponse("<h1>About</h1>")
--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages import views
+from pages.views import home_view, contact_view, social_view, about_view
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('', home_view, name='home'),
+    path('contact/', contact_view,),
+    path('social/', social_view,),
+    path('about/', about_view,),
     path('admin/', admin.site.urls),
 ]
