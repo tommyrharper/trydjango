30a3eff062e1abe6e87ab352481d6b66b878a7e9
tommyrharper <thomasrobertharper@gmail.com>
2021-06-09 17:41:54 +0100

    Add request to view parameters

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,14 +3,17 @@ from django.http.request import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs): # *args, **kwargs
+def home_view(request, *args, **kwargs): # *args, **kwargs
+  print('request: ', request)
+  print('args: ', args, 'kwargs: ', kwargs)
+  print(request.user)
   return HttpResponse("<h1>Hello World</h1>") # string of HTML code
 
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
   return HttpResponse("<h1>Contact</h1>")
 
-def social_view(*args, **kwargs):
+def social_view(request, *args, **kwargs):
   return HttpResponse("<h1>Social</h1>")
 
-def about_view(*args, **kwargs):
+def about_view(request, *args, **kwargs):
   return HttpResponse("<h1>About</h1>")
